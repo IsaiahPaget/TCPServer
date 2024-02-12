94415b0b8bd04df99c18d4d5da3d4107c9505d78
Isaiah Paget <113209222+IsaiahPaget@users.noreply.github.com>
2024-02-11 16:34:34 -0800

    put socket in passive mode

--- server.c
+++ server.c
@@ -2,6 +2,7 @@
 #include <arpa/inet.h>
 #include <stdio.h>
 #define PORT 5555
+#define BACKLOG 10
 /**
 * Encapsulates the properties of  the server
 */
@@ -13,21 +14,9 @@ typedef struct Server {
 
 // creates a socket for the servers and makes it passive so we can wait for connections on it later.
 int server_listen(server_t* server) {
-
-}
-
-// accepts new connections and then prints 'hello, world' to them.
-int server_accept(server_t* server) {
-
-}
-
-int main(int argc, char *argv[])
-{
     int err = 0;
-    server_t server = { 0 };
-
     // get socket and store it in server
-    err = (server.listen_fd = socket(AF_INET, SOCK_STREAM, 0));
+    err = (server->listen_fd = socket(AF_INET, SOCK_STREAM, 0));
     if (err == -1) {
         perror("socket");
         printf("Failed to create socket endpoint\n");
@@ -41,7 +30,7 @@ int main(int argc, char *argv[])
 
     // bind and address to the socket
     err = bind(
-        server.listen_fd,
+        server->listen_fd,
         (struct sockaddr*)&server_addr,
         sizeof(server_addr)
     );
@@ -50,6 +39,28 @@ int main(int argc, char *argv[])
         printf("Failed to bind socket to address\n");
         return err;
     }
+
+    // make socket passive with listen
+    err = listen(
+        server->listen_fd,
+        BACKLOG
+    );
+    if (err == -1) {
+        perror("listen");
+        printf("Failed to put socket in passive mode\n");
+        return err;
+    }
+}
+
+// accepts new connections and then prints 'hello, world' to them.
+int server_accept(server_t* server) {
+
+}
+
+int main(int argc, char *argv[])
+{
+    int err = 0;
+    server_t server = { 0 };
 
     err = server_listen(&server);
     if (err) {
