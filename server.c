164965779f450d98354b41288beabcab368e62f5
Isaiah Paget <113209222+IsaiahPaget@users.noreply.github.com>
2024-02-11 16:14:26 -0800

    opened socket and binded it to and address

---
+++ server.c
@@ -0,0 +1,69 @@
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#define PORT 5555
+/**
+* Encapsulates the properties of  the server
+*/
+typedef struct Server {
+    // file descriptor of the socket in passive
+    // mode to wait for connections
+    int listen_fd;
+} server_t;
+
+// creates a socket for the servers and makes it passive so we can wait for connections on it later.
+int server_listen(server_t* server) {
+
+}
+
+// accepts new connections and then prints 'hello, world' to them.
+int server_accept(server_t* server) {
+
+}
+
+int main(int argc, char *argv[])
+{
+    int err = 0;
+    server_t server = { 0 };
+
+    // get socket and store it in server
+    err = (server.listen_fd = socket(AF_INET, SOCK_STREAM, 0));
+    if (err == -1) {
+        perror("socket");
+        printf("Failed to create socket endpoint\n");
+        return err;
+    }
+
+    struct sockaddr_in server_addr = { 0 };
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(PORT);
+
+    // bind and address to the socket
+    err = bind(
+        server.listen_fd,
+        (struct sockaddr*)&server_addr,
+        sizeof(server_addr)
+    );
+    if (err == -1) {
+        perror("bind");
+        printf("Failed to bind socket to address\n");
+        return err;
+    }
+
+    err = server_listen(&server);
+    if (err) {
+        printf("Failed to listen on address 0.0.0.0:%d\n", PORT);
+        return err;
+    }
+
+    for (;;) {
+        err = server_accept(&server);
+        if (err) {
+            printf("Failed accepting connection\n");
+            return err;
+        }
+    }
+
+    return 0;
+}
